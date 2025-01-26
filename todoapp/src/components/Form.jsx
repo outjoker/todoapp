1434d0acc30b143332537a6bfdaaf963d30a126e
outjoker <balasrinivas12@gmail.com>
2025-01-26 07:05:10 +0530

    marked the todo items as completed or not completed by introducing strike through

--- todoapp/src/components/Form.jsx
+++ todoapp/src/components/Form.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import styles from "./form.module.css"
 export default function Form({ todolist,  setTodolist }) {
-    const [todo, setTodo] = useState("");
+    //const [todo, setTodo] = useState("");
+    // previously we were just saving the todo item name alone 
+    // if we want to now store the state of the todo item whether it is completed 
+    // or not we need to change the datastructure of useState("") from string to object
+    //hence commenting the above & declaring newly
+    const [todo, setTodo]=useState({name: "", completed: false})
 
     function handleSubmit(e) {
         e.preventDefault()
         // using spread operator to hold the old items in the todoslist
         setTodolist([...todolist, todo])
-        console.log(todolist)
-        //setting input text to empty
-        setTodo("")
+     
+        //setting input text to empty after entering the todo item
+        setTodo({name: "", completed: false})
     }
+    
     return (
 
         <form className={styles.todoform} onSubmit={handleSubmit}>
@@ -18,8 +24,8 @@ export default function Form({ todolist,  setTodolist }) {
                 <input className={styles.moderninput}
                     placeholder="Enter the ToDo item"
                     type="text"
-                    value={todo}
-                    onChange={(e) => setTodo(e.target.value)}>
+                    value={todo.name}
+                    onChange={(e) => setTodo({name: e.target.value, completed: false})}>
                 </input>
                 <button className={styles.modernbutton} type="submit">Add</button>
 
